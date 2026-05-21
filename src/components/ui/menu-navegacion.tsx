c05f8c8af013fe9a4f39761397d7fcb4368fe119
Rodrigo Emerson Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2026-05-21 12:44:01 -0500

    refactor(ui): traducir todas las funciones y componentes exportados al español

--- src/components/ui/menu-navegacion.tsx
+++ src/components/ui/menu-navegacion.tsx
@@ -2,28 +2,28 @@ import * as React from "react";
 import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu";
 import { concatenarClases as cn } from "@/lib/utiles";
 
-export const NavigationMenu = React.forwardRef<
+export const MenuNavegacion = React.forwardRef<
   React.ComponentRef<typeof NavigationMenuPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Root>
 >(({ className, ...props }, ref) => (
   <NavigationMenuPrimitive.Root ref={ref} className={cn("nav-menu", className)} {...props} />
 ));
-NavigationMenu.displayName = NavigationMenuPrimitive.Root.displayName;
+MenuNavegacion.displayName = NavigationMenuPrimitive.Root.displayName;
 
-export const NavigationMenuList = React.forwardRef<
+export const ListaMenuNavegacion = React.forwardRef<
   React.ComponentRef<typeof NavigationMenuPrimitive.List>,
   React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.List>
 >(({ className, ...props }, ref) => (
   <NavigationMenuPrimitive.List ref={ref} className={cn("nav-menu-list", className)} {...props} />
 ));
-NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName;
+ListaMenuNavegacion.displayName = NavigationMenuPrimitive.List.displayName;
 
-export const NavigationMenuItem = NavigationMenuPrimitive.Item;
+export const ElementoMenuNavegacion = NavigationMenuPrimitive.Item;
 
-export const NavigationMenuLink = React.forwardRef<
+export const EnlaceMenuNavegacion = React.forwardRef<
   React.ComponentRef<typeof NavigationMenuPrimitive.Link>,
   React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Link>
 >(({ className, ...props }, ref) => (
   <NavigationMenuPrimitive.Link ref={ref} className={cn("nav-menu-link", className)} {...props} />
 ));
-NavigationMenuLink.displayName = NavigationMenuPrimitive.Link.displayName;
+EnlaceMenuNavegacion.displayName = NavigationMenuPrimitive.Link.displayName;
--- src/components/ui/pestanas.tsx
+++ src/components/ui/pestanas.tsx
@@ -2,26 +2,26 @@ import * as React from "react";
 import * as TabsPrimitive from "@radix-ui/react-tabs";
 import { concatenarClases as cn } from "@/lib/utiles";
 
-export const Tabs = TabsPrimitive.Root;
+export const Pestanas = TabsPrimitive.Root;
 
-export const TabsList = React.forwardRef<
+export const ListaPestanas = React.forwardRef<
   React.ComponentRef<typeof TabsPrimitive.List>,
   React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
 >(({ className, ...props }, ref) => <TabsPrimitive.List ref={ref} className={cn("tabs-list", className)} {...props} />);
-TabsList.displayName = TabsPrimitive.List.displayName;
+ListaPestanas.displayName = TabsPrimitive.List.displayName;
 
-export const TabsTrigger = React.forwardRef<
+export const DisparadorPestanas = React.forwardRef<
   React.ComponentRef<typeof TabsPrimitive.Trigger>,
   React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
 >(({ className, ...props }, ref) => (
   <TabsPrimitive.Trigger ref={ref} className={cn("tabs-trigger", className)} {...props} />
 ));
-TabsTrigger.displayName = TabsPrimitive.Trigger.displayName;
+DisparadorPestanas.displayName = TabsPrimitive.Trigger.displayName;
 
-export const TabsContent = React.forwardRef<
+export const ContenidoPestanas = React.forwardRef<
   React.ComponentRef<typeof TabsPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
 >(({ className, ...props }, ref) => (
   <TabsPrimitive.Content ref={ref} className={cn("tabs-content", className)} {...props} />
 ));
-TabsContent.displayName = TabsPrimitive.Content.displayName;
+ContenidoPestanas.displayName = TabsPrimitive.Content.displayName;

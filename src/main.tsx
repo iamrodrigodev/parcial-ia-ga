5e81c6b2e40a854d203cb1ec3e1246485e6c6a0b
Rodrigo Emerson Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2026-05-21 12:06:53 -0500

    style: eliminar theme.css y usar unicamente scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Aplicacion } from "@/app/Aplicacion";
-import "@/styles/theme.css";
 import "@/styles/globals.scss";
 
 createRoot(document.getElementById("root")!).render(

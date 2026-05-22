22f41a6c8d6098e3bbb9e52f7fa49daea792bd7e
rodrigodev.ts <rodrigodev.ts@gmail.com>
2026-05-21 22:00:24 -0500

    feat: implementar modulo interactivo resolutor de laberinto con algoritmo genetico y visualizacion optimizada de pasos

--- src/routes/enrutador.tsx
+++ src/routes/enrutador.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import { PlantillaPrincipal } from "@/layouts/PlantillaPrincipal";
 import { PaginaInicio } from "@/pages/PaginaInicio";
 import { PaginaOperadores } from "@/pages/PaginaOperadores";
+import { PaginaLaberinto } from "@/pages/PaginaLaberinto";
 import { PaginaNoEncontrada } from "@/pages/PaginaNoEncontrada";
 
 export const enrutador = createBrowserRouter([
@@ -17,6 +18,10 @@ export const enrutador = createBrowserRouter([
       {
         path: "operadores",
         element: <PaginaOperadores />,
+      },
+      {
+        path: "laberinto",
+        element: <PaginaLaberinto />,
       }
     ],
   },
